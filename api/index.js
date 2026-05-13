dc35247a2ab863414f7c94ad68d9c8f73fda9a9c
IvaniltonsSilva <contato.ivaniltoncordeiro@gmail.com>
2026-05-13 13:12:52 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=MDISMAA0FUX&afp_campaign=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=NJBYMO32BIP&afp_campaign=oa' });
     res.end();
   }
