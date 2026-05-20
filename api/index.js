daacb47b709ccbcc633803f44bf459b8af26abb0
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-20 12:07:08 -0300

    chore: update shareCode to 19N6WZZ9RI8

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=TZCRS32VUKS&afp_campaign=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=19N6WZZ9RI8&afp_campaign=oa' });
     res.end();
   }
