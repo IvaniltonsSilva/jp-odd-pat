ef091e64cd442c7edbc52f211a0e2c34fd0797a1
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-27 17:02:44 -0300

    chore: update shareCode to I23DIZWKNC9

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=VYU22TH6U14&afp_campaign=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=I23DIZWKNC9&afp_campaign=oa' });
     res.end();
   }
